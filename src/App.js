7b79fe93adb1b432f876413ce6bfc944bc66af4f
Danishkhilji <danishkhilji112@gmail.com>
2023-07-26 23:32:07 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThingSpeakDataFetcher from './components/fetchData';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My ThingSpeak App</h1>
+      <ThingSpeakDataFetcher />
+    </div>
+  );
+};
+
+export default App;
